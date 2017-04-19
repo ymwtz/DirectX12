c9323f7ff238c5078aca471750a76cd975618dcb
ymwtz <ymwtz@DESKTOP-IEFV58V>
2017-04-19 18:46:27 +0800

    Jish's laptop

--- DirectX12/Specific/BlendDemo/Shaders/Main.hlsl
+++ DirectX12/Specific/BlendDemo/Shaders/Main.hlsl
@@ -55,7 +55,7 @@ cbuffer cbPass : register(b1)
     float4 gFogColor;
     float gFogStart;
     float gFogRange;
-    //float2 cbPerObjectPad2;
+    float2 cbPerObjectPad2;
 
     // Indices [0, NUM_DIR_LIGHTS) are directional lights;
     // indices [NUM_DIR_LIGHTS, NUM_DIR_LIGHTS+NUM_POINT_LIGHTS) are point lights;
